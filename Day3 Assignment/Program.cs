fae0ea5d324f2fcde48585cc63068f410e45db03
Z1758 <timeofwitch@gmail.com>
2024-07-18 16:52:48 +0900

    Initial commit

---
+++ Day3 Assignment/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+
+namespace Day3_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 과제1
+            int successCount;
+            bool exceptionFlag;
+
+            Console.WriteLine("성공한 갯수를 입력하여 주세요");
+
+            exceptionFlag = int.TryParse(Console.ReadLine(), out successCount);
+            if (exceptionFlag)
+            {
+                if (successCount > 100)
+                {
+                    Console.WriteLine("100 초과 예외처리");
+                }
+                else if (successCount == 100)
+                {
+                    Console.WriteLine("SS 등급입니다.");
+                }
+                else if (successCount >= 90)
+                {
+                    Console.WriteLine("S 등급입니다.");
+                }
+                else if (successCount >= 70)
+                {
+                    Console.WriteLine("A 등급입니다.");
+                }
+                else if (successCount >= 40)
+                {
+                    Console.WriteLine("B 등급입니다.");
+                }
+                else
+                {
+                    Console.WriteLine("F 등급입니다.");
+                }
+
+
+                /*
+                
+                else if (successCount >= 0)
+                {
+                    Console.WriteLine("F 등급입니다.");
+                }
+                else
+                {
+                    Console.WriteLine("0 미만 예외처리");
+                }
+
+                */
+            }
+            else
+            {
+                Console.WriteLine("바른 수 입력 예외처리");
+            }
+
+            #endregion
+
+            #region 심화 과제2
+            int num;
+          
+            Console.WriteLine("정수 입력");
+
+            int.TryParse(Console.ReadLine(), out num);
+
+            Console.WriteLine();
+
+            //if문
+            Console.WriteLine("if문");
+            if (num == 1)
+            {
+                Console.WriteLine("Cocked Pistol 발령");
+            }
+            else if (num == 2)
+            {
+                Console.WriteLine("Fast Pace 발령");
+            }
+            else if (num == 3)
+            {
+                Console.WriteLine("Round House 발령");
+            }
+            else
+            {
+                Console.WriteLine("비상 태세");
+            }
+
+            Console.WriteLine();
+
+            //switch문
+            Console.WriteLine("switch문");
+            switch (num)
+            {
+                case 1:
+                    Console.WriteLine("Cocked Pistol 발령");
+                    break;
+                case 2:
+                    Console.WriteLine("Fast Pace 발령");
+                    break;
+                case 3:
+                    Console.WriteLine("Round House 발령");
+                    break;
+
+                default:
+                    Console.WriteLine("비상 태세");
+                    break;
+            }
+            #endregion
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿namespace Day3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region 과제1
+
+            string nameInput;
+            Console.WriteLine("이름 입력");
+            
+            nameInput = Console.ReadLine();
+
+            //자리 표시자
+            Console.WriteLine("{0}님, 반갑습니다.", nameInput);
+
+            #endregion
+
+            #region 과제2
+
+            float fNum1;
+            float fNum2;
+                                  
+            Console.WriteLine("첫 번째 실수를 입력하여 주세요");
+            float.TryParse(Console.ReadLine(), out fNum1);
+
+            Console.WriteLine("두 번째 실수를 입력하여 주세요");
+            float.TryParse(Console.ReadLine(), out fNum2);   
+
+            Console.WriteLine("두 수의 합은 {0} 입니다", fNum1+fNum2);
+
+            #endregion
+
+            #region 과제3
+
+            int iNum1;
+            int iNum2;
+
+            Console.Write("나눗셈을 진행할 첫 번째 수를 입력하여 주세요: ");
+            int.TryParse(Console.ReadLine(), out iNum1);
+
+            Console.Write("두번째 나눌 수를 입력해주세요: ");
+            int.TryParse(Console.ReadLine(), out iNum2);
+
+            //문자열 보간 $
+            Console.WriteLine($"{iNum1}와 {iNum2}의 나눗셈 결과, 몫은 {iNum1 / iNum2} 나머지는 {iNum1 % iNum2}");
+
+            #endregion
+
+            #region 과제4
+
+            int threeNum1;
+            int threeNum2;
+            int threeNum3;
+
+            string testString;
+
+            //Parse
+            Console.WriteLine("첫째 정수 입력");
+            testString = Console.ReadLine();
+            threeNum1 = int.Parse(testString);
+
+            //TryParse
+            Console.WriteLine("둘째 정수 입력");
+            int.TryParse(Console.ReadLine(), out threeNum2);
+
+            //Convert
+            Console.WriteLine("셋째 정수 입력");
+            testString = Console.ReadLine();
+            threeNum3 = Convert.ToInt32(testString);
+
+            Console.WriteLine($"첫째수 더하기 둘째수에 셋째수를 곱한 값은 {(threeNum1+threeNum2)*threeNum3} 입니다.");
+
+            #endregion
+        }
+    }
+}
